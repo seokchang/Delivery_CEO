751190409891c06666b595063940ff55256bb066
seohong <seokchang_h@naver.com>
2020-11-22 20:01:14 +0900

    Added Project & .gitignore set

---
+++ deliveryCEO/WebContent/WEB-INF/views/common/msg.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%
+	String msg = (String) request.getAttribute("msg");
+	String loc = (String) request.getAttribute("loc");
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<script type="text/javascript">
+		alert("<%=msg%>");
+		location.href = "<%=loc%>";
+	</script>
+</body>
+</html>
---
+++ deliveryCEO/src/common/JDBCTemplate.java
@@ -0,0 +1,90 @@
+package common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCTemplate {
+	public static Connection getConnection() {
+		Connection conn = null;
+
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+		String dbID = "";
+		String dbPW = "";
+
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, dbID, dbPW);
+			conn.setAutoCommit(false);
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	public static void close(Connection conn) {
+		try {
+			if (conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void close(Statement stmt) {
+		try {
+			if (stmt != null && !stmt.isClosed()) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void close(ResultSet rset) {
+		try {
+			if (rset != null && !rset.isClosed()) {
+				rset.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void commit(Connection conn) {
+		try {
+			if (conn != null && !conn.isClosed()) {
+				conn.commit();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void rollback(Connection conn) {
+		try {
+
+			if (conn != null && !conn.isClosed()) {
+				conn.rollback();
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
